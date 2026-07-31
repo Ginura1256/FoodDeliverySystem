fd9d504fd4303c02d8907e899b95edd604b2a2ea
IT24102441 <it24102441@my.sliit.lk>
2026-08-01 00:58:12 +0530

    Update Swagger UI endpoint configuration in Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -19,10 +19,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FoodDelivery API v1");
+    });
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 app.MapControllers();
 
 var summaries = new[]
